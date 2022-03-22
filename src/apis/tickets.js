d95eb783329b5f6d68b7713e6803bc211ac8d79a
Adriana Lazurca <adrianar.lazurca@gmail.com>
2022-03-22 15:37:07 +0100

    [Kanban-5]: display ticket details page

--- src/apis/tickets.js
+++ src/apis/tickets.js
@@ -1,30 +1,41 @@
 import axios from 'axios';
 
 export const getTickets = async () => {
+   const url = 'http://localhost:3001/tickets';
    try {
-      const { data } = await axios.get('http://localhost:3001/tickets');
+      const { data } = await axios.get(url);
       return data;
    } catch (error) {
       console.error('No tickets found!', error);
    }
 };
 
-export const getTicket = async (userId) => {
+export const getTicket = async (ticketId) => {
+   const url = `http://localhost:3001/tickets/${ticketId}`;
    try {
-      const { data } = await axios.get(`http://localhost:3001/users/?id=${userId}`);
+      const { data } = await axios.get(url);
       return data;
    } catch (error) {
-      console.error('No tickets found!', error);
+      console.error('No ticket found!', error);
    }
 };
 
 export const createTicket = async (ticket) => {
    const url = 'http://localhost:3001/tickets';
-
    try {
       const { data } = await axios.post(url, ticket);
       return data;
    } catch (error) {
-      console.error('There was an error!', error);
+      console.error("Can't create ticket!", error);
+   }
+};
+
+export const updateTicket = async (ticketId, updtatedTicket) => {
+   const url = `http://localhost:3001/tickets/${ticketId}`;
+   try {
+      const { data } = await axios.patch(url, updtatedTicket);
+      return data;
+   } catch (error) {
+      console.error("Can't update ticket!", error);
    }
 };
--- src/components/ticket-details/TicketDetails.jsx
+++ src/components/ticket-details/TicketDetails.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import { getTicket } from '../../apis/tickets';
+
 export const TicketDetails = () => {
+   const [ticket, setTicket] = useState();
    const { ticketId } = useParams();
 
-   return <div>Ticket with number {ticketId}</div>;
+   useEffect(() => {
+      const fetchTicket = async () => {
+         const data = await getTicket(ticketId);
+         setTicket(data);
+      };
+      fetchTicket();
+   }, [ticketId]);
+
+   return (
+      <>
+         {ticket && (
+            <div className='bg-light p-2 my-2'>
+               <h2 className='text-start fw-lighter'>{ticket.title}</h2>
+               <div>
+                  <h6>Details</h6>
+                  <p className='fs-6 p-1'>{`Type: ${ticket.type}`}</p>
+                  <p className='fs-6 p-1'>{`Priority: ${ticket.priority}`}</p>
+                  <p className='fs-6 p-1'>{`Status: ${ticket.status}`}</p>
+               </div>
+
+               <div>
+                  <h6>Description</h6>
+                  <p>{ticket.description}</p>
+               </div>
+            </div>
+         )}
+      </>
+   );
 };
