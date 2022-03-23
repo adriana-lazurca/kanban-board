9247711986b5262c443cfbfffd4e354ff1e382dc
Adriana Lazurca <adrianar.lazurca@gmail.com>
2022-03-23 19:54:21 +0100

    [Kanban-6]: implement drag and drop on the tickets

--- src/components/board-column/BoardColumn.jsx
+++ src/components/board-column/BoardColumn.jsx
@@ -1,3 +1,5 @@
+import { Draggable } from 'react-beautiful-dnd';
+
 import { TicketCard } from '../ticket-card/TicketCard';
 import './board-column.scss';
 
@@ -6,11 +8,36 @@ export const BoardColumn = ({ tickets }) => {
       <div className='container-fluid'>
          <div className='row'>
             <div className='board-column col mx-2 py-2'>
-               {tickets.map((ticket) => (
-                  <TicketCard key={ticket.id} ticket={ticket} />
+               {tickets.map((ticket, index) => (
+                  <DraggableItem id={ticket.id} index={index} key={ticket.id}>
+                     <TicketCard key={ticket.id} ticket={ticket} />
+                  </DraggableItem>
                ))}
             </div>
          </div>
       </div>
+   );
+};
+
+const DraggableItem = ({ id, index, children }) => {
+   return (
+      <Draggable draggableId={id} index={index} key={id}>
+         {(provided, snapshot) => {
+            return (
+               <div
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  style={{
+                     ...provided.draggableProps.style,
+                     backgroundColor: snapshot.isDragging ? '#263B4A' : '#456C86',
+                     userSelect: 'none',
+                  }}
+               >
+                  {children}
+               </div>
+            );
+         }}
+      </Draggable>
    );
 };
--- src/components/desktop-board/DesktopBoard.jsx
+++ src/components/desktop-board/DesktopBoard.jsx
@@ -1,20 +1,119 @@
+import { useEffect, useState } from 'react';
+import { v4 as uuid } from 'uuid';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+
 import { BoardColumn } from '../board-column/BoardColumn';
 
 export const DesktopBoard = ({ columns, tickets }) => {
-   return (
-      <div className='container-fluid'>
-         <div className='row'>
-            {columns.map((column, index) => {
-               const columnTickets = tickets.filter((ticket) => ticket.status === column.name);
+   const [containers, setContainers] = useState();
 
-               return (
-                  <div className='col' key={`column-${column.name}-${index}`}>
-                     <h6 className='text-center'>{column.title}</h6>
-                     <BoardColumn tickets={columnTickets} selectedColumnName={column.name} columnTitle={column.title} />
-                  </div>
-               );
-            })}
+   const onStatusChange = (status) => {
+      
+   };
+
+   useEffect(() => {
+      const items = tickets.map((ticket) => ({
+         ...ticket,
+         id: ticket.id.toString(),
+      }));
+
+      const containers = {};
+
+      columns.forEach((column) => {
+         const columnItems = items.filter((item) => item.status === column.name);
+
+         containers[uuid()] = {
+            ...column,
+            items: columnItems,
+         };
+      });
+
+      setContainers(containers);
+   }, [columns, tickets]);
+
+   return (
+      <div className='container-fluid pb-5'>
+         <div className='row'>
+            <DragDropContext onDragEnd={(result) => onDragEnd(result, containers, setContainers, onStatusChange)}>
+               {containers &&
+                  Object.entries(containers).map(([containerId, container]) => {
+                     return (
+                        <div className='col' key={containerId}>
+                           <h6 className='text-center'>{container.title}</h6>
+
+                           <DroppableContainer id={containerId}>
+                              <BoardColumn
+                                 tickets={container.items}
+                                 selectedColumnName={container.name}
+                                 columnTitle={container.title}
+                              />
+                           </DroppableContainer>
+                        </div>
+                     );
+                  })}
+            </DragDropContext>
          </div>
       </div>
    );
+};
+
+const DroppableContainer = ({ id, children }) => {
+   return (
+      <Droppable key={id} droppableId={id}>
+         {(provided, snapshot) => {
+            return (
+               <div
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  style={{
+                     background: snapshot.isDraggingOver ? 'lightblue' : 'lightgrey',
+                  }}
+               >
+                  {children}
+                  {provided.placeholder}
+               </div>
+            );
+         }}
+      </Droppable>
+   );
+};
+
+const onDragEnd = (result, columns, setColumns, onStatusChange) => {
+   if (!result.destination) return;
+   const { source, destination } = result;
+
+   if (source.droppableId !== destination.droppableId) {
+      // drag to another container
+      const sourceColumn = columns[source.droppableId];
+      const destColumn = columns[destination.droppableId];
+      const sourceItems = [...sourceColumn.items];
+      const destItems = [...destColumn.items];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+      setColumns({
+         ...columns,
+         [source.droppableId]: {
+            ...sourceColumn,
+            items: sourceItems,
+         },
+         [destination.droppableId]: {
+            ...destColumn,
+            items: destItems,
+         },
+      });
+      onStatusChange(destColumn.name);
+   } else {
+      // drag inside the same container
+      const column = columns[source.droppableId];
+      const copiedItems = [...column.items];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setColumns({
+         ...columns,
+         [source.droppableId]: {
+            ...column,
+            items: copiedItems,
+         },
+      });
+   }
 };
